e8a1dd5d4689be4a5fb5473e3deac60f1451fc3f
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-18 22:06:32 +0700

    add new helper

---
+++ lib/helper/stringHelper.ts
@@ -0,0 +1,9 @@
+function toTitleCase(str: string) {
+	return str.replace(/\w\S*/g, function (txt: string) {
+		return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
+	})
+}
+
+export const stringHelper = {
+    toTitleCase
+}
