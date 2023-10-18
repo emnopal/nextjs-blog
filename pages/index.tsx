a1c5233d6287b9824fe728e4f5f8c238f3bb0a8e
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-18 22:07:12 +0700

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import Link from 'next/link';
 
 import { userService } from '@/services/usersService';
+import { stringHelper } from '@/lib/helper/stringHelper';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+
+    const [nameStr, setNameStr] = useState<string | null>(null)
+
+    useEffect(() => {
+        const origNameStr = userService.userValue?.name
+        const nameStrTitle = stringHelper.toTitleCase(origNameStr)
+        setNameStr(nameStrTitle)
+    }, []);
+
     return (
         <div className="p-4">
             <div className="container">
-                <h1 style={{color: 'black'}}>Hi {userService.userValue?.name}!</h1>
-                <p style={{color: 'black'}}>You&lsquo;re logged in with Next.js & JWT!!</p>
-                <p style={{color: 'black'}}><Link href="/users">Manage Users</Link></p>
+                <h1 >Hi {nameStr}!</h1>
+                <p >You&lsquo;re logged in with Next.js & JWT!!</p>
+                <p ><Link href="/users">Manage Users</Link></p>
             </div>
         </div>
     );
