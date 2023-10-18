b16d92cb6d5a9418d5441182e78f6d8c4027133d
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-18 22:04:41 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
 /** @type {import('next').NextConfig} */
 
 if (!process.env.SECRET) {
-  throw "Please provide secret first"
+	throw 'Please provide secret first'
 }
 
 const nextConfig = {
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-      connectionString: process.env.MONGODB_URI ?? "mongodb://localhost/nextjs-example01",
-      secret: process.env.SECRET,
-      environmentMode: process.env.NODE_ENV,
-      saltLength: 10,
-  },
-  publicRuntimeConfig: {
-      apiUrl: {
-        v1: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000/api/v1' // development api
-        : 'http://localhost:3000/api/v1' // production api
-      },
-  },
+	reactStrictMode: true,
+	serverRuntimeConfig: {
+		connectionString:
+			process.env.MONGODB_URI ?? 'mongodb://localhost/nextjs-example01',
+		secret: process.env.SECRET,
+		environmentMode: process.env.NODE_ENV,
+	},
+	publicRuntimeConfig: {
+		apiUrl: {
+			v1:
+				process.env.NODE_ENV === 'development'
+					? 'http://localhost:3000/api/v1' // development api
+					: 'http://localhost:3000/api/v1', // production api
+		},
+	},
 }
 
 module.exports = nextConfig
