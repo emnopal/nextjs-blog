4579e29dc4be01520d15c4e231b5962c2dc0d304
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-18 22:06:26 +0700

    update nav link

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 
 interface INavLink {
     children: ReactNode
     href: string
     exact?: boolean
-    className?: string;
+    className?: string
+    style?: CSSProperties
 }
 
 export function NavLink({ children, href, exact, ...props }: INavLink): JSX.Element {
@@ -23,7 +24,7 @@ export function NavLink({ children, href, exact, ...props }: INavLink): JSX.Elem
 
 NavLink.propTypes = {
     href: PropTypes.string.isRequired,
-    exact: PropTypes.bool
+    exact: PropTypes.bool,
 };
 
 NavLink.defaultProps = {
