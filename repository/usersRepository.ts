73a47ebc8b4887db4777e9da4c4e3213b4a4e0bf
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-19 21:55:33 +0700

    fully changed to mongodb (without orm or mongoose)

--- repository/usersRepository.ts
+++ repository/usersRepository.ts
@@ -1,93 +1,138 @@
 import getConfig from 'next/config'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-import { mongodb as db } from '@/lib/config/mongo'
+import { ObjectId, type Db } from 'mongodb'
+import normalizeEmail from 'validator/lib/normalizeEmail'
 
 const { serverRuntimeConfig } = getConfig()
-const User = db.User
 
-interface IAuth {
+export interface IUserModel {
+	_id: ObjectId
 	username: string
+	name: string
+	email: string
 	password: string
+	role: string
+	isDeleting?: boolean
 }
 
-interface IAuthResponse {
-	id: string
+// still string since Auth User parameters are from local storage
+export interface ICurrentAuthUserModel {
+	_id: string
 	username: string
-	email: string
 	name: string
-	createdAt: Date
-	updatedAt: Date
+	email: string
+	password: string
+	role: string
 	token: string
 }
 
-async function authenticate({
-	username,
-	password,
-}: IAuth): Promise<IAuthResponse> {
-	const user = await User.findOne({ username })
+interface IAuthResponse {
+	user?: IUserModel
+	password?: string | undefined
+	token?: string
+}
 
-	if (!(user && bcrypt.compareSync(password, user.password))) {
-		throw 'Username or password is incorrect'
+async function authenticate(
+	db: Db,
+	username: string,
+	password: string,
+): Promise<IAuthResponse | null> {
+	const usersCollection = db.collection<IUserModel>('users')
+	const user = await usersCollection.findOne({ username })
+
+	if (user && (await bcrypt.compare(password, user.password))) {
+		const token = jwt.sign({ sub: user._id }, serverRuntimeConfig.secret, {
+			expiresIn: '7d',
+		})
+		return { ...user, password: undefined, token } // filtered out password
 	}
 
-	const token = jwt.sign({ sub: user.id }, serverRuntimeConfig.secret, {
-		expiresIn: '7d',
-	})
-
-	const { id, email, name, createdAt, updatedAt } = user.toJSON()
-
-	return {
-		id,
-		username,
-		email,
-		name,
-		createdAt,
-		updatedAt,
-		token,
-	}
+	throw 'Wrong Credentials'
 }
 
-async function getAll(): Promise<any> {
-	return await User.find()
+async function getAll(db: Db): Promise<IUserModel[]> {
+	const usersCollection = db.collection<IUserModel>('users')
+	return await usersCollection.find({}).toArray()
 }
 
-async function getById(id: string): Promise<any> {
-	return await User.findById(id)
+async function getById(db: Db, id: string): Promise<IUserModel | null> {
+	const usersCollection = db.collection<IUserModel>('users')
+	return await usersCollection.findOne({ _id: new ObjectId(id) })
 }
 
-async function create(params: any): Promise<void> {
-	if (await User.findOne({ username: params.username })) {
+async function create(db: Db, params: IUserModel): Promise<IUserModel> {
+	const usersCollection = db.collection<IUserModel>('users')
+
+	if (await usersCollection.findOne({ username: params.username })) {
 		throw 'Username "' + params.username + '" is already taken'
 	}
-
-	const user = new User(params)
 
 	if (params.password) {
-		user.password = bcrypt.hashSync(params.password, 10)
+		params.password = await bcrypt.hash(params.password, 10)
 	}
 
-	await user.save()
+	params.role = 'user'
+
+	const { insertedId } = await usersCollection.insertOne(params)
+	params._id = insertedId
+	return params
 }
 
-async function update(id: string, params: any): Promise<void> {
-	const user = await User.findById(id)
+async function update(
+	db: Db,
+	id: string,
+	params: Partial<IUserModel>,
+): Promise<void> {
 
-	if (!user) throw 'User not found'
-	if (
-		user.username !== params.username &&
-		(await User.findOne({ username: params.username }))
-	) {
-		throw 'Username "' + params.username + '" is already taken'
+	const usersCollection = db.collection<IUserModel>('users')
+	const existingUser = await usersCollection.findOne({ _id: new ObjectId(id) })
+
+	if (!existingUser) {
+		throw 'User not found'
 	}
 
-	Object.assign(user, params)
+	if (existingUser.username !== params.username) {
+		const userWithSameUsername = await usersCollection.findOne({
+			username: params.username,
+		})
 
-	await user.save()
+		if (userWithSameUsername) {
+			throw 'Username "' + params.username + '" is already taken'
+		}
+	}
+
+	return await usersCollection
+		.findOneAndUpdate(
+			{ _id: new ObjectId(id) },
+			{ $set: params },
+			{ returnDocument: 'after', projection: { password: 0 } },
+		)
+		.then(({ value }: any) => value)
 }
 
-async function _delete(id: string): Promise<void> {
-	await User.findByIdAndRemove(id)
+async function updatePassword(
+	db: Db,
+	id: string,
+	oldPassword: string,
+	newPassword: string,
+) {
+	const usersCollection = db.collection<IUserModel>('users')
+	const user = await usersCollection.findOne(new ObjectId(id))
+	if (!user) return false
+	const matched = await bcrypt.compare(oldPassword, user.password)
+	if (!matched) return false
+	const password = await bcrypt.hash(newPassword, 10)
+	await usersCollection.updateOne(
+		{ _id: new ObjectId(id) },
+		{ $set: { password } },
+	)
+	return true
+}
+
+async function _delete(db: Db, id: string): Promise<void> {
+	const usersCollection = db.collection<IUserModel>('users')
+	await usersCollection.deleteOne({ _id: new ObjectId(id) })
 }
 
 export const usersRepository = {
@@ -96,5 +141,6 @@ export const usersRepository = {
 	getById,
 	create,
 	update,
+	updatePassword,
 	delete: _delete,
 }
