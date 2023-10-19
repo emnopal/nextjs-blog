3c0293583bac26abc6042efb670f6428b507c26d
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-19 20:56:44 +0700

    add db connection to each api

--- pages/api/v1/users/[id].ts
+++ pages/api/v1/users/[id].ts
@@ -1,42 +1,44 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { apiHandler } from '@/lib/handler/handler';
-import { usersRepository } from '@/repository/usersRepository';
+import { apiHandler } from '@/lib/handler/handler'
+import { usersRepository } from '@/repository/usersRepository'
+import { getMongoDb } from '@/lib/config/mongo'
 
 async function getById(req: NextApiRequest, res: NextApiResponse) {
+	const db = await getMongoDb()
 
-    const { id } = req.query
+	const { id } = req.query
 
-    if (id) {
-        const user = await usersRepository.getById(id as string);
-        if (!user) throw 'User Not Found';
-        return res.status(200).json(user);
-    }
-
+	if (id) {
+		const user = await usersRepository.getById(db, id as string)
+		if (!user) throw 'User Not Found'
+		return res.status(200).json(user)
+	}
 }
 
 async function update(req: NextApiRequest, res: NextApiResponse) {
+	const db = await getMongoDb()
 
-    const { id } = req.query
+	const { id } = req.query
 
-    if (id) {
-        await usersRepository.update(id as string, req.body);
-        return res.status(200).json({});
-    }
-
+	if (id) {
+		await usersRepository.update(db, id as string, req.body)
+		return res.status(200).json({})
+	}
 }
 
 async function _delete(req: NextApiRequest, res: NextApiResponse) {
+	const db = await getMongoDb()
 
-    const { id } = req.query
+	const { id } = req.query
 
-    if (id) {
-        await usersRepository.delete(id as string);
-        return res.status(200).json({});
-    }
+	if (id) {
+		await usersRepository.delete(db, id as string)
+		return res.status(200).json({})
+	}
 }
 
 export default apiHandler({
-    GET: getById,
-    PUT: update,
-    DELETE: _delete
-});
+	GET: getById,
+	PUT: update,
+	DELETE: _delete,
+})
