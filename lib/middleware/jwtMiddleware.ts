1d5918763f9757698bef6e0b8906cfcce8b4fbbc
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-19 20:55:06 +0700

    fix promisify on promise return function depreciation due to unhandle rejection

--- lib/middleware/jwtMiddleware.ts
+++ lib/middleware/jwtMiddleware.ts
@@ -1,19 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { expressjwt } from 'express-jwt';
-import util from 'util';
-import getConfig from 'next/config';
+import { expressjwt } from 'express-jwt'
+import util from 'util'
+import getConfig from 'next/config'
 
-const { serverRuntimeConfig } = getConfig();
+const { serverRuntimeConfig } = getConfig()
 
-export { jwtMiddleware };
+export function jwtMiddleware(req: NextApiRequest, res: NextApiResponse) {
 
-function jwtMiddleware(req: NextApiRequest, res: NextApiResponse) {
-    const middleware = expressjwt({ secret: serverRuntimeConfig.secret, algorithms: ['HS256'] }).unless({
-        path: [
-            '/api/v1/users/register',
-            '/api/v1/users/authenticate'
-        ]
-    });
+    const middleware = expressjwt({
+		secret: serverRuntimeConfig.secret,
+		algorithms: ['HS256'],
+	}).unless({
+		path: ['/api/v1/users/register', '/api/v1/users/authenticate'],
+	})
 
-    return util.promisify(middleware)(req, res);
+	return middleware(req, res, () => {}) // todo: handle rejected promise, currently leave it empty
 }
