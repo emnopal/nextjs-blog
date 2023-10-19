9c618c2b256ec489042dc437a363741e67bb452c
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-19 08:45:58 +0700

    add toggle for enable and disable form

--- components/users/Edit.tsx
+++ components/users/Edit.tsx
@@ -11,6 +11,8 @@ import { UserValidation } from '@/lib/validation/users';
 
 export function EditUser(props: any) {
     const user = props?.user;
+    const disableForms = props?.disableForms ?? true
+    const isProfile = props?.isProfile ?? false
     const router = useRouter();
 
     const validationSchema = UserValidation.EditUser()
@@ -43,29 +45,36 @@ export function EditUser(props: any) {
             <div className="row">
                 <div className="mb-3 col">
                     <label className="form-label">Name</label>
-                    <input type="text" {...register('name')} className={`form-control ${errors.name ? 'is-invalid' : ''}`} />
+                    <input type="text" {...register('name')} className={`form-control ${errors.name ? 'is-invalid' : ''}`} disabled={disableForms} />
                     <div className="invalid-feedback">{errors.name?.message?.toString()}</div>
                 </div>
                 <div className="mb-3 col">
                     <label className="form-label">Email</label>
-                    <input type="text" {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`} />
+                    <input type="text" {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`} disabled={disableForms} />
                     <div className="invalid-feedback">{errors.email?.message?.toString()}</div>
                 </div>
             </div>
             <div className="row">
                 <div className="mb-3 col">
                     <label className="form-label">Username</label>
-                    <input type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
+                    <input type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} disabled={disableForms} />
                     <div className="invalid-feedback">{errors.username?.message?.toString()}</div>
                 </div>
             </div>
             <div className="mb-3">
-                <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary me-2">
-                    {formState.isSubmitting && <span className="spinner-border spinner-border-sm me-1"></span>}
-                    Save
-                </button>
-                <button onClick={() => reset(formOptions.defaultValues)} type="button" disabled={formState.isSubmitting} className="btn btn-secondary">Reset</button>
-                <Link href="/users" className="btn btn-link">Cancel</Link>
+                {!disableForms ?
+                    <>
+                        <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary me-2">
+                            {formState.isSubmitting && <span className="spinner-border spinner-border-sm me-1"></span>}
+                            Save
+                        </button>
+                        <button onClick={() => reset(formOptions.defaultValues)} type="button" disabled={formState.isSubmitting} className="btn btn-danger">Reset</button>
+                    </>
+                    :
+                    ''
+                }
+                {isProfile ? '' : <Link href="/users" className="btn btn-link">Back to User List</Link> }
+                <Link href="#" className="btn btn-link">Change Password</Link>
             </div>
         </form>
     );
---
+++ pages/users/[id].tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import { EditUser } from '@/components/users/Edit';
+import { Layout } from '@/components/users/Layout';
+import { Spinner } from '@/components/Spinner';
+import { alertHelper } from '@/lib/helper/alert';
+import { userService } from '@/services/usersService';
+import { stringHelper } from '@/lib/helper/stringHelper';
+
+
+export default function Edit() {
+
+    const router = useRouter();
+    const { query } = router;
+    const [user, setUser] = useState();
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    useEffect(() => {
+        const { id, enable_edit } = query;
+        if (!id) return;
+
+        if (enable_edit) setIsDisabled(false)
+
+        // fetch user and set default form values if in edit mode
+        userService.getById(id as string)
+            .then(x => setUser(x))
+            .catch(alertHelper.error)
+    }, [query]);
+
+    const usernameStr = user?.username ?? ''
+    const origNameStr = user?.name ?? ''
+    const nameStr = stringHelper.toTitleCase(origNameStr)
+
+    const setEditable = () => {
+        setIsDisabled(false);
+    };
+
+    const setDisabled = () => {
+        setIsDisabled(true);
+    };
+
+    return (
+        <Layout>
+            <h1>
+                {nameStr} &nbsp;
+                {isDisabled ?
+                    <button onClick={setEditable} className="btn btn-primary btn-sm">
+                        Edit
+                    </button> : <button onClick={setDisabled} className="btn btn-warning btn-sm">
+                        Cancel
+                    </button>
+                }
+            </h1>
+            {user ? <EditUser user={user} disableForms={isDisabled} /> : <Spinner />}
+        </Layout>
+    );
+}
