2974e8d3586ac211af9f0c9dcd74b968e8cfa9de
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-20 22:44:21 +0700

    improve homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,48 @@ import Link from 'next/link'
 import { userService } from '@/services/users'
 import { stringHelper } from '@/lib/helper/stringHelper'
 import { useEffect, useState } from 'react'
+import { getCookie } from 'cookies-next'
 
 export default function Home() {
 	const [nameStr, setNameStr] = useState<string | null>(null)
+	const [isAuthorized, setAuthorized] = useState(false)
 
 	useEffect(() => {
-		const origNameStr = userService.userValue?.name
-		const nameStrTitle = stringHelper.toTitleCase(origNameStr)
-		setNameStr(nameStrTitle)
-	}, [])
+		const isToken = getCookie('jwtToken') ?? ''
+		if (userService.userValue && isToken) {
+			setAuthorized(true)
+		}
+		if (isAuthorized) {
+			const origNameStr = userService.userValue?.name
+			const nameStrTitle = stringHelper.toTitleCase(origNameStr)
+			setNameStr(nameStrTitle)
+		}
+	}, [isAuthorized])
 
-	return (
+	let homeTemplate = (
 		<div className="p-4">
 			<div className="container">
-				<h1>Hi {nameStr}!</h1>
-				<p>You&lsquo;re logged in with Next.js & JWT!!</p>
-				<p>
-					<Link href="/users">Manage Users</Link>
-				</p>
+				<h1>Please login to continue</h1>
+				<Link href="/login">Login Here</Link>
 			</div>
 		</div>
 	)
+
+	console.log(isAuthorized)
+	if (isAuthorized) {
+		console.log('authorized')
+		homeTemplate = (
+			<div className="p-4">
+				<div className="container">
+					<h1>Hi {nameStr}!</h1>
+					<p>You&lsquo;re logged in with Next.js & JWT!!</p>
+					<p>
+						<Link href="/users">Manage Users</Link>
+					</p>
+				</div>
+			</div>
+		)
+	}
+
+	return <>{homeTemplate}</>
 }
