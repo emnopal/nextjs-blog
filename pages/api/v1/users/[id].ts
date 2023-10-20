52eb46e2e9e11d65e74d383c774cc0359a66503f
Muhammad Naufal <mnaufal0999@gmail.com>
2023-10-20 22:43:16 +0700

    add roles

--- pages/api/v1/users/[id].ts
+++ pages/api/v1/users/[id].ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { apiHandler } from '@/lib/handler/handler'
 import { usersRepository } from '@/repository/users'
 import { getMongoDb } from '@/lib/config/mongo'
+import jwt from 'jsonwebtoken'
 
 async function getById(req: NextApiRequest, res: NextApiResponse) {
 	const db = await getMongoDb()
@@ -9,9 +10,17 @@ async function getById(req: NextApiRequest, res: NextApiResponse) {
 	const { id } = req.query
 
 	if (id) {
-		const user = await usersRepository.getById(db, id as string)
-		if (!user) throw 'User Not Found'
-		return res.status(200).json(user)
+		try {
+			const decodedToken = jwt.decode(req.headers.authorization?.split(' ').pop() as string) as jwt.JwtPayload
+			if (decodedToken.id == id || decodedToken.role === 'superadmin') {
+				const user = await usersRepository.getById(db, id as string)
+				if (!user) throw 'User Not Found'
+				return res.status(200).json(user)
+			}
+			throw 'User Not Found'
+		} catch (error) {
+			throw error
+		}
 	}
 }
 
--- pages/api/v1/users/index.ts
+++ pages/api/v1/users/index.ts
@@ -2,11 +2,17 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { apiHandler } from '@/lib/handler/handler'
 import { usersRepository } from '@/repository/users'
 import { getMongoDb } from '@/lib/config/mongo'
+import jwt from 'jsonwebtoken'
 
 async function getAll(req: NextApiRequest, res: NextApiResponse) {
+	const decodedToken = jwt.decode(req.headers.authorization?.split(' ').pop() as string) as jwt.JwtPayload
 	const db = await getMongoDb()
-	const users = await usersRepository.getAll(db)
-	return res.status(200).json(users)
+	const user = await usersRepository.getById(db, decodedToken.id)
+	if (decodedToken.role === 'superadmin') {
+		const users = await usersRepository.getAll(db)
+		return res.status(200).json(users)
+	}
+	return res.status(200).json([user])
 }
 
 export default apiHandler({
